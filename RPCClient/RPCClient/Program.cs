0452165a11adc82aca43999b0081228e8ebc2946
GROOVETECH\cuonglam <cuong.lam@groovetechnology.com>
2018-05-31 17:11:19 +0700

    add

---
+++ RPCClient/RPCClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            do
+            {
+                var rpcClient = new RpcClientClass();
+
+                Console.WriteLine(" [x] Requesting fib " + i);
+                var response = rpcClient.Call(i.ToString());
+                Console.WriteLine(" [.] Got '{0}'", response);
+
+                rpcClient.Close();
+                i++;
+            } while (i <1000);
+            
+
+            Console.Read();
+        }
+    }
+}
